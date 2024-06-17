b0a45db7da2a93bedf7b6fc8954f1031041f0cc8
Satyam Gupta <satyamgupta1495@gmail.com>
2024-06-17 18:42:00 +0530

    fixed build issues

--- client/src/components/products/ProductDetails.tsx
+++ client/src/components/products/ProductDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import useProducts from '../hooks/useProducts';
 import useStore from '@/store/useStore';
